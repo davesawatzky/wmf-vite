86c79356554f802e7041d4f4f1e240beafab6e6a
David Sawatzky <david@diatonic.io>
2022-12-16 16:04:32 -0600

    closed all forms

--- src/composables/openClosed.ts
+++ src/composables/openClosed.ts
@@ -1,4 +1,4 @@
 export const soloOpen = false
-export const groupOpen = true
-export const schoolOpen = true
-export const communityOpen = true
+export const groupOpen = false
+export const schoolOpen = false
+export const communityOpen = false
